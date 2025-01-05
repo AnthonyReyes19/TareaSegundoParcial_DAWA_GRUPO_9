6ecd01363dd4d83094d7518e873876fded9fed6d
AnthonyReyes19 <anthony2003reyes@hotmail.com>
2025-01-05 18:37:27 -0500

    eliminacion de carpetas innecesarias

--- tareasegundo-app/src/app/app.routes.ts
+++ tareasegundo-app/src/app/app.routes.ts
@@ -1,6 +1,4 @@
 import { Routes } from '@angular/router';
-import { ListaLibrosComponent } from './components/lista-libros/lista-libros.component';
-import { CrudLibrosComponent } from './components/crud-libros/crud-libros.component';
 import { LoginComponent } from './components/login/login.component';
 import { LogsComponent } from './components/logs/logs.component';
 import { IndexComponent } from './components/index/index.component';
