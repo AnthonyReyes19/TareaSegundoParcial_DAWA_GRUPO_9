d1e954474adc674976bf5406549eaf0935484052
AnthonyReyes19 <anthony2003reyes@hotmail.com>
2025-01-05 19:32:40 -0500

    eliminacion de lineas y agragacion de otros2

--- tareasegundo-app/src/app/services/logging.service.ts
+++ tareasegundo-app/src/app/services/logging.service.ts
@@ -4,19 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 
-// Define la clase del servicio de logging
 export class LoggingService {
-  // Array privado que almacenará todos los logs 
+  // Array que almacenará todos los logs 
   private logs: any[] = [];
 
   // Método para registrar un nuevo evento
-  // Recibe el nombre del evento y los detalles asociados
   logEvent(event: string, details: any) {
-    // Crea un objeto log combinando el evento y sus detalles
     const log = { event, details };
     // Añade el log al array de logs
     this.logs.push(log);
-    // Muestra el log en la consola del navegador para debugging y posterior muestra en una tabla
     console.log('Log recorded:', log);
   }
 
